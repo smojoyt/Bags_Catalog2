c01d473f252cd20e0951c33dee4af5399b3821ec
justinlmcneal <mcneal.justin99@gmail.com>
2025-05-12 17:59:28 -0400

    dw

---
+++ api/create-checkout-session.js
@@ -0,0 +1,29 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+    if (req.method === 'POST') {
+        try {
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: [{
+                    price_data: {
+                        currency: 'usd',
+                        product_data: { name: 'Test Product' },
+                        unit_amount: 1999,
+                    },
+                    quantity: 1,
+                }],
+                mode: 'payment',
+                success_url: `${req.headers.origin}/public/success.html`,
+                cancel_url: `${req.headers.origin}/public/cancel.html`,
+            });
+
+            res.status(200).json({ url: session.url });
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+};
